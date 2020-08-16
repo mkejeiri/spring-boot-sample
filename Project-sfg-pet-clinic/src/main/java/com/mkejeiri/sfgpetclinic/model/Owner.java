9816b709c80d5bb66b53f8c6080f4c338fc0ddeb
mkejeiri <kejeiri@gmail.com>
2020-08-16 19:11:34 +0200

    Added POJO's. Closes #2.

---
+++ Project-sfg-pet-clinic/src/main/java/com/mkejeiri/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.mkejeiri.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ Project-sfg-pet-clinic/src/main/java/com/mkejeiri/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.mkejeiri.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+}
