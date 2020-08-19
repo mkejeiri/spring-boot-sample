1ffd96da750488eb4af923e40511a76ace07cfa0
mkejeiri <kejeiri@gmail.com>
2020-08-19 21:20:59 +0200

    Listed all Owners on Owners Index page, closes #16

--- Project-sfg-pet-clinic/pet-clinic-web/src/main/java/com/mkejeiri/sfgpetclinic/controllers/OwnerController.java
+++ Project-sfg-pet-clinic/pet-clinic-web/src/main/java/com/mkejeiri/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package com.mkejeiri.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mkejeiri.sfgpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService; 
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		
+		var owners = ownerService.findAll();
+		model.addAttribute("owners",owners);
 		return "owners/index";
 	}
 }
