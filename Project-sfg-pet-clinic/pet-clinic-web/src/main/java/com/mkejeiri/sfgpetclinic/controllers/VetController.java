dee76d0bee424352155b97be7ce53e43a7423642
mkejeiri <kejeiri@gmail.com>
2020-08-19 21:37:48 +0200

    Listed all Vets on index page, closes #15

--- Project-sfg-pet-clinic/pet-clinic-web/src/main/java/com/mkejeiri/sfgpetclinic/controllers/VetController.java
+++ Project-sfg-pet-clinic/pet-clinic-web/src/main/java/com/mkejeiri/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.mkejeiri.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mkejeiri.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
-	
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
+	public String listVets(Model model) {
+		var vets = vetService.findAll();
+		model.addAttribute("vets", vets);
 		return "vets/index";
 	}
 
