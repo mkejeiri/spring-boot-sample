0463c2e01a0058f34fda35e16413b9185d6585ef
mkejeiri <kejeiri@gmail.com>
2020-08-21 23:34:51 +0200

    Create PetType Map Service Impl, closes #29

---
+++ Project-sfg-pet-clinic/pet-clinic-data/src/main/java/com/mkejeiri/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.mkejeiri.sfgpetclinic.services;
+
+import com.mkejeiri.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ Project-sfg-pet-clinic/pet-clinic-data/src/main/java/com/mkejeiri/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,40 @@
+package com.mkejeiri.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.mkejeiri.sfgpetclinic.model.PetType;
+import com.mkejeiri.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType petType) {
+		return super.save(petType);
+	}
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public void delete(PetType petType) {
+		super.delete(petType);
+
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+
+	}
+
+}
