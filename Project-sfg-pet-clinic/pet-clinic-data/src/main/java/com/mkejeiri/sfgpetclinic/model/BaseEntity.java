16639357d81e4ef42c6e97c5981a9f0726d7bded
mkejeiri <kejeiri@gmail.com>
2020-08-22 22:24:06 +0200

    Implement Base Entity (JPA), closes #35

--- Project-sfg-pet-clinic/pet-clinic-data/src/main/java/com/mkejeiri/sfgpetclinic/model/BaseEntity.java
+++ Project-sfg-pet-clinic/pet-clinic-data/src/main/java/com/mkejeiri/sfgpetclinic/model/BaseEntity.java
@@ -2,7 +2,15 @@ package com.mkejeiri.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //i.e. not mapped to the db, only subclasses will have a table
 public class BaseEntity implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
@@ -11,5 +19,9 @@ public class BaseEntity implements Serializable {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+	
+	public boolean isNew() {
+		return this.id == null;
 	}
 }
