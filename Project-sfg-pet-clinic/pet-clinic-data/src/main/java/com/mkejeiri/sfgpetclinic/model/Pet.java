ce6da7245634df027142d1de45d7714e64ca67ad
mkejeiri <kejeiri@gmail.com>
2020-08-29 18:07:00 +0200

    Added functionality to Add or Update a Visit, closes #56

--- Project-sfg-pet-clinic/pet-clinic-data/src/main/java/com/mkejeiri/sfgpetclinic/model/Pet.java
+++ Project-sfg-pet-clinic/pet-clinic-data/src/main/java/com/mkejeiri/sfgpetclinic/model/Pet.java
@@ -36,8 +36,10 @@ public class Pet extends BaseEntity {
 		this.petType = petType;
 		this.owner = owner;
 		this.birthDate = birthDate;
-		this.visits = visits;		
+		if (visits == null || visits.size() > 0) {
+			this.visits = visits;
 		}
+	}
 
 	@Column(name = "name")
 	private String name;
@@ -55,7 +57,7 @@ public class Pet extends BaseEntity {
 	private LocalDate birthDate;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
-	 @Builder.Default private Set<Visit> visits  = new HashSet<Visit>() ;
+	private Set<Visit> visits  = new HashSet<Visit>() ;
 
 //	public String getName() {
 //		return name;
