c0a6059665da74bcb88768f63c54b3abcf5eacfd
mkejeiri <kejeiri@gmail.com>
2020-08-30 17:35:19 +0200

    added Vets JSON Endpoint for demo

--- Project-sfg-pet-clinic/pet-clinic-web/src/main/java/com/mkejeiri/sfgpetclinic/controllers/VetController.java
+++ Project-sfg-pet-clinic/pet-clinic-web/src/main/java/com/mkejeiri/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,18 @@
 package com.mkejeiri.sfgpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mkejeiri.sfgpetclinic.model.Vet;
 import com.mkejeiri.sfgpetclinic.services.VetService;
 
 @Controller
-@RequestMapping({ "/vets","/vets.html"})
+//@RequestMapping({"/vets","/vets.html"})
 public class VetController {
 	private final VetService vetService;
 
@@ -15,11 +20,17 @@ public class VetController {
 		this.vetService = vetService;
 	}
 
-	@RequestMapping({ "","/",  "/index", "/index.html" })
+	// old -> @RequestMapping({ "","/", "/index", "/index.html" })
+	@GetMapping({ "/vets", "/vets/", "/vets/index", "/vets/index.html" })
 	public String listVets(Model model) {
 		var vets = vetService.findAll();
 		model.addAttribute("vets", vets);
 		return "vets/index";
+	}
+
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		return vetService.findAll();
 	}
 
 }
